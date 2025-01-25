170fd1fda19b042277f92604654b3d8b3b875b4b
unknown <paul-emmanuel.buffe@laplateforme.io>
2025-01-25 13:57:11 +0100

    changes on menu retun win

--- pendu_paul.py
+++ pendu_paul.py
@@ -409,12 +409,13 @@ def main():
                 screen.blit(win_message, (200, 500))
                 screen.blit(score_message, (200, 550))
 
-                # Display the "Return to Menu" button
-                back_button = pygame.Rect(350, 600, 150, 50)
-                pygame.draw.rect(screen, (200, 200, 200), back_button)
-                back_text = ubuntu_font.render("Return to Menu", True, black)
-                back_rect = back_text.get_rect(center=back_button.center)
+                 # Display the "Return to Menu" button
+                back_button = pygame.Rect(30, 40, 150, 50)
+                reduced_font = pygame.font.Font(None, 30)
+                back_text = reduced_font.render("Return to Menu", True, black)
+                back_rect = back_text.get_rect(center=(80, 60))
                 screen.blit(back_text, back_rect)
+                pygame.display.flip()
 
                 pygame.display.flip()
 
